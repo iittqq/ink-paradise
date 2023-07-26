ae5af396df4be0f8e4baeb4dce293ad87f4d16c6
ikaika lee <leeikaika4@gmail.com>
2023-07-26 14:00:35 -0500

    more setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Toolbar, Typography, Divider, Button } from "@mui/material";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container
+			disableGutters
+			sx={{ minWidth: "100%", backgroundColor: "#181716" }}
+		>
+			<Toolbar sx={{ justifyContent: "space-between" }}>
+				<div>
+					<Typography color='white'>Ink Paradise</Typography>
+					<Typography color='white'>Hello Degenerate</Typography>
+				</div>
+				<Button>Recent Updates</Button>
+				<Button>Recent Updates</Button>
+				<Button>Recent Updates</Button>
+			</Toolbar>
+			<Divider sx={{ height: 3, bgcolor: "#8A6240" }} />
+			<HashRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</HashRouter>
+		</Container>
+	);
 }
 
 export default App;
