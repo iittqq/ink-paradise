18e4d86073c0cf4b2f099382b3de30661a2d204b
ikaika lee <leeikaika4@gmail.com>
2023-09-18 20:50:28 -0500

    skeleton done

--- src/APIs/MangaDexAPI.ts
+++ src/APIs/MangaDexAPI.ts
@@ -1,7 +1,8 @@
 const baseUrl = "https://api.mangadex.org/";
 
 export const CoverById = (coverArt: any) => `${baseUrl}cover/${coverArt}`;
-
+export const FeedById = (id: any) =>
+	`${baseUrl}manga/${id}/feed?limit=100&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&includeFutureUpdates=1&order%5Bchapter%5D=desc`;
 export const DetailsById = (id: any) => `${baseUrl}manga/${id}`;
 export const RecentlyUpdated = () =>
 	`${baseUrl}manga?limit=20&offset=0&includedTagsMode=AND&excludedTagsMode=OR&publicationDemographic%5B%5D=none&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&contentRating%5B%5D=pornographic&order%5BlatestUploadedChapter%5D=desc`;
--- src/Pages/IndividualManga.tsx
+++ src/Pages/IndividualManga.tsx
@@ -9,9 +9,11 @@ import {
 	Button,
 	Typography,
 } from "@mui/material";
-import { DetailsById } from "../APIs/MangaDexAPI";
+import { DetailsById, FeedById } from "../APIs/MangaDexAPI";
 import dayjs from "dayjs";
 import Header from "../Components/Header";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
 var utc = require("dayjs/plugin/utc");
 dayjs.extend(utc);
 
@@ -27,7 +29,7 @@ const IndividualManga = () => {
 	const [mangaDetails, setMangaDetails] = useState(Object);
 	const [mangaName, setMangaName] = useState();
 	const [mangaDescription, setMangaDescription] = useState();
-	const [mangaAltTitles, setMangaAltTitles] = useState();
+	const [mangaAltTitles, setMangaAltTitles] = useState<Object[]>([]);
 	const [mangaLanguages, setMangaLanguages] = useState<string[]>([]);
 	const [mangaContentRating, setMangaContentRating] = useState();
 	const [mangaCreatedAt, setMangaCreatedAt] = useState();
@@ -39,28 +41,57 @@ const IndividualManga = () => {
 	const [mangaRelationships, setMangaRelationships] = useState();
 	const [mangaType, setMangaType] = useState();
 	const [mangaLatest, setMangaLatest] = useState();
+	const [mangaFeed, setMangaFeed] = useState<Object[]>([]);
 
 	const fetchRecentlyUpdatedManga = async () => {
-		const { data } = await axios.get(DetailsById(state.id));
-		setMangaDetails(data.data);
-		console.log(data.data);
-		setMangaName(data.data["attributes"].title["en"]);
-		setMangaDescription(data.data["attributes"].description["en"]);
-		setMangaAltTitles(data.data["attributes"].altTitles);
-		setMangaLanguages(data.data["attributes"].availableTranslatedLanguages);
-		setMangaContentRating(data.data["attributes"].contentRating);
-		setMangaCreatedAt(data.data["attributes"].createdAt);
-		setMangaRaw(data.data["attributes"].links["raw"]);
-		setMangaState(data.data["attributes"].state);
-		setMangaStatus(data.data["attributes"].status);
-		setMangaTags(data.data["attributes"].tags);
-		setMangaUpdatedAt(data.data["attributes"].updatedAt);
-		setMangaRelationships(data.data["relationships"]);
-		setMangaLatest(data.data["attributes"].latestUploadedChapter);
+		const { data: details } = await axios.get(DetailsById(state.id));
+		setMangaDetails(details.data);
+		console.log(details.data);
+		setMangaName(details.data["attributes"].title["en"]);
+		setMangaDescription(details.data["attributes"].description["en"]);
+		setMangaAltTitles(details.data["attributes"].altTitles);
+		setMangaLanguages(details.data["attributes"].availableTranslatedLanguages);
+		setMangaContentRating(details.data["attributes"].contentRating);
+		setMangaCreatedAt(details.data["attributes"].createdAt);
+		setMangaRaw(details.data["attributes"].links["raw"]);
+		setMangaState(details.data["attributes"].state);
+		setMangaStatus(details.data["attributes"].status);
+		setMangaTags(details.data["attributes"].tags);
+		setMangaUpdatedAt(details.data["attributes"].updatedAt);
+		setMangaRelationships(details.data["relationships"]);
+		setMangaLatest(details.data["attributes"].latestUploadedChapter);
 	};
 
+	const fetchMangaFee = async () => {
+		const { data: feed } = await axios.get(FeedById(state.id));
+		console.log(feed.data);
+		setMangaFeed(feed.data);
+	};
+
+	const getCountryDetails = (code: string) => {
+		switch (code) {
+			case "en":
+				return "us";
+
+			case "ja":
+				return "jp";
+
+			case "pt-br":
+				return "pt";
+
+			case "uk":
+				return "gb";
+
+			case "es-la":
+				return "es";
+			default:
+				//statements;
+				return code;
+		}
+	};
 	useEffect(() => {
 		fetchRecentlyUpdatedManga();
+		fetchMangaFee();
 	}, []);
 
 	return (
@@ -80,17 +111,24 @@ const IndividualManga = () => {
 				<Grid item sx={{ width: "100%" }}>
 					<Header />
 				</Grid>
-				<Grid item sx={{ width: "95%" }}>
+				<Grid
+					item
+					sx={{
+						width: "95%",
+						height: { xs: "250px", sm: "280px", lg: "350px" },
+					}}
+				>
 					<Grid
 						sx={{
 							color: "white",
+							height: "100%",
 						}}
 						container
 						direction='row'
 						justifyContent='space-evenly'
 						alignItems='center'
 					>
-						<Grid item sx={{}}>
+						<Grid item>
 							<Card
 								sx={{
 									height: {
@@ -125,17 +163,42 @@ const IndividualManga = () => {
 						</Grid>
 						<Grid item sx={{ width: { xs: "70%", md: "70%", lg: "80%" } }}>
 							<div>
-								<Typography
-									sx={{
-										display: "-webkit-box",
-										overflow: "hidden",
-										WebkitBoxOrient: "vertical",
-										WebkitLineClamp: 2,
-										fontSize: { xs: 20, sm: 25, lg: 30 },
+								<div
+									style={{
+										display: "flex",
+										flexDirection: "row",
+										justifyContent: "flex-start",
+										alignItems: "flex-end",
 									}}
 								>
-									{mangaName}
-								</Typography>
+									<Typography
+										sx={{
+											display: "-webkit-box",
+											overflow: "hidden",
+											WebkitBoxOrient: "vertical",
+											WebkitLineClamp: 2,
+											fontSize: { xs: 20, sm: 25, lg: 30 },
+											paddingRight: "20px",
+										}}
+									>
+										{mangaName}
+									</Typography>
+
+									{mangaAltTitles.map((current) => (
+										<Typography
+											sx={{
+												display: "-webkit-box",
+												overflow: "hidden",
+												WebkitBoxOrient: "vertical",
+												WebkitLineClamp: 2,
+												fontSize: { xs: 0, sm: 9, lg: 10 },
+												paddingRight: "5px",
+											}}
+										>
+											/ {Object.values(current)}
+										</Typography>
+									))}
+								</div>
 								<Typography
 									sx={{
 										display: "-webkit-box",
@@ -162,69 +225,110 @@ const IndividualManga = () => {
 										Show more
 									</Button>
 								</div>
-								<Typography
-									align='center'
-									sx={{ fontSize: { xs: 12, sm: 14, lg: 16 } }}
-								>
-									Languages
-								</Typography>
-								<Grid
-									container
-									direction='row'
-									justifyContent='center'
-									alignItems='center'
-									sx={{ paddingTop: "10px" }}
-								>
-									{mangaLanguages.map((current) => (
-										<Grid item>
-											<Button
-												sx={{
-													fontSize: { xs: 8, sm: 10, lg: 12 },
-													color: "#777777",
-													"&:hover": {
-														backgroundColor: "transparent",
-													},
-												}}
-												size='small'
-											>
-												{current}
-											</Button>
-										</Grid>
-									))}
-								</Grid>
+								<div>
+									<Typography
+										sx={{
+											color: "#555555",
+											fontSize: { xs: 8, sm: 10, lg: 15 },
+										}}
+									>
+										Content Rating:
+									</Typography>
+									<Typography
+										sx={{
+											color: "#555555",
+											fontSize: { xs: 8, sm: 10, lg: 15 },
+										}}
+									>
+										{mangaContentRating}
+									</Typography>
+								</div>
 							</div>
 						</Grid>
 					</Grid>
 				</Grid>
 
-				<Grid item sx={{ width: "100%" }}>
+				<Grid
+					item
+					sx={{
+						width: "95%",
+						height: { xs: "230px", sm: "100px", lg: "200px" },
+					}}
+				>
 					<Grid
 						container
-						direction='row'
+						direction='column'
 						justifyContent='center'
 						alignItems='center'
 						spacing={1}
 					>
-						{mangaTags.map((current: any) => (
-							<Grid item>
-								<Button
-									sx={{
-										backgroundColor: "#191919",
-										width: { xs: "110px", sm: "110px", lg: "110px" },
-										height: { xs: "20px", sm: "20px", lg: "20px" },
-										textAlign: "center",
-									}}
-								>
-									<Typography
-										noWrap
-										color='#333333'
-										sx={{ fontSize: { xs: 9, sm: 10, lg: 10 } }}
-									>
-										{current["attributes"].name["en"]}
-									</Typography>
-								</Button>
+						<Grid item>
+							<Typography
+								align='center'
+								color='white'
+								sx={{ fontSize: { xs: 12, sm: 14, lg: 16 } }}
+							>
+								Languages
+							</Typography>
+						</Grid>
+						<Grid item>
+							<Grid
+								container
+								direction='row'
+								justifyContent='center'
+								alignItems='center'
+								sx={{}}
+								spacing={1}
+							>
+								{mangaLanguages.map((current) => (
+									<Grid item>
+										<Button
+											sx={{
+												backgroundColor: "#191919",
+												width: { xs: "20px", sm: "20px", lg: "20px" },
+												height: { xs: "30px", sm: "30px", lg: "30px" },
+											}}
+										>
+											<span
+												style={{ height: "20px", width: "20px" }}
+												className={`fi fi-${getCountryDetails(current)}`}
+											></span>
+										</Button>
+									</Grid>
+								))}
 							</Grid>
-						))}
+						</Grid>
+						<Grid item>
+							<Grid
+								container
+								direction='row'
+								justifyContent='center'
+								alignItems='center'
+								sx={{}}
+								spacing={1}
+							>
+								{mangaTags.map((current: any) => (
+									<Grid item>
+										<Button
+											sx={{
+												backgroundColor: "#191919",
+												width: { xs: "110px", sm: "110px", lg: "110px" },
+												height: { xs: "20px", sm: "20px", lg: "20px" },
+												textAlign: "center",
+											}}
+										>
+											<Typography
+												noWrap
+												color='#333333'
+												sx={{ fontSize: { xs: 9, sm: 10, lg: 10 } }}
+											>
+												{current["attributes"].name["en"]}
+											</Typography>
+										</Button>
+									</Grid>
+								))}
+							</Grid>
+						</Grid>
 					</Grid>
 					<Grid item sx={{ paddingTop: "20px" }}>
 						<Typography
@@ -271,6 +375,50 @@ const IndividualManga = () => {
 								RAW
 							</Typography>
 						</Button>
+					</Grid>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						width: { xs: "80%", md: "50%", lg: "30%" },
+						height: { xs: "330px", md: "300px", lg: "320px" },
+						overflow: "auto",
+					}}
+				>
+					<Grid
+						container
+						direction='column'
+						justifyContent='center'
+						alignItems='center'
+						spacing={1}
+					>
+						{mangaFeed.map((current: any) => (
+							<Grid item sx={{ width: "100%", height: "50px" }}>
+								<Button
+									sx={{
+										width: "100%",
+										color: "white",
+										height: "100%",
+										backgroundColor: "#333333",
+										justifyContent: "flex-start",
+									}}
+								>
+									<Typography sx={{ textTransform: "none" }}>
+										Chapter {current["attributes"].chapter}
+									</Typography>
+									<span
+										style={{
+											height: "10px",
+											width: "10px",
+											paddingLeft: "20px",
+										}}
+										className={`fi fi-${getCountryDetails(
+											current["attributes"].translatedLanguage
+										)}`}
+									></span>
+								</Button>
+							</Grid>
+						))}
 					</Grid>
 				</Grid>
 			</Grid>
