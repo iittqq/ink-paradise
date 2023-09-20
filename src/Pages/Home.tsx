b10dc5537a24fa22a526ef4a16426ac03d18564c
Adam's PC <ajdupuy15@gmail.com>
2023-09-20 14:08:52 -0500

    Added Recently Added List to home Page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,6 +5,7 @@ import { RecentlyUpdated } from "../APIs/MangaDexAPI";
 import axios from "axios";
 import RecentlyUpdatedCarousel from "../Components/RecentlyUpdatedCarousel";
 import RecentlyUpdatedList from "../Components/RecentlyUpdatedList";
+import RecentlyAddedList from "../Components/RecentlyAddedList";
 
 const noFilter = ["safe", "suggestive", "erotica", "pornographic"];
 const baseUrl = "https://api.mangadex.org";
@@ -48,6 +49,29 @@ const Home = () => {
 					{/**<RecentlyUpdatedCarousel />*/}
 
 					<RecentlyUpdatedList />
+
+				</Grid>
+			</Grid>
+			<Grid
+				container
+				direction='column'
+				justifyContent='space-evenly'
+				alignItems='center'
+			>
+				<Grid item sx={{paddingTop: "20px"}}>
+					<Typography sx={{ color: "white" }}>Recently Added</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						width: "100%",
+						display: "flex",
+						justifyContent: "center",
+					}}
+				>
+
+					<RecentlyAddedList />
+
 				</Grid>
 			</Grid>
 		</Container>
