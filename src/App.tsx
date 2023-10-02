8d5ad6d75c7e8b79a340c3b809a693d879bb9d44
ikaika lee <leeikaika4@gmail.com>
2023-10-02 12:38:51 -0500

    chapter offset implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import "./App.css";
-import { Container, Toolbar, Typography, Divider, Button } from "@mui/material";
+import { Container } from "@mui/material";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
 import IndividualManga from "./Pages/IndividualManga";
 import RecentlyAdded from "./Pages/RecentlyAdded";
 import SearchResults from "./Pages/SearchResults";
 import ListOfMangaPage from "./Pages/ListOfMangaPage";
-import axios from "axios";
-import { useState, useEffect } from "react";
 
 //#121212
 
-const baseUrl = "https://api.mangadex.org";
-
 function App() {
-	const [recentlyUpdatedMangaDetails, setRecentlyUpdatedMangaDetails] =
-		useState<Object[]>([]);
-
-	const fetchRecentlyAddedManga = async () => {
-		const { data } = await axios.get(`${baseUrl}/manga`, {
-			params: {
-				order: {
-					latestUploadedChapter: "desc",
-				},
-				limit: 50,
-				contentRating: ["safe", "suggestive", "erotica"],
-			},
-		});
-		setRecentlyUpdatedMangaDetails(data.data);
-
-		console.log(data.data);
-	};
-
-	useEffect(() => {
-		fetchRecentlyAddedManga();
-	}, []);
 	return (
 		<Container
 			disableGutters
@@ -53,12 +28,7 @@ function App() {
 					<Route path='/results' element={<SearchResults />}></Route>
 					<Route
 						path='/mangaList'
-						element={
-							<ListOfMangaPage
-								title='Recently Updated'
-								mangaData={recentlyUpdatedMangaDetails}
-							/>
-						}
+						element={<ListOfMangaPage title='Recently Updated' />}
 					></Route>
 				</Routes>
 			</BrowserRouter>
