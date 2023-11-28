a291d688c4a1400b8da2c828e624f529cd1a220d
ikaika lee <leeikaika4@gmail.com>
2023-11-28 14:46:01 -0600

    better account page. added more user details

--- src/Pages/Account.tsx
+++ src/Pages/Account.tsx
@@ -3,6 +3,7 @@ import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
+import { STATIC_EXECUTION_CONTEXT } from "styled-components/dist/constants";
 
 const baseUrlMangaDex = "https://api.mangadex.org";
 const baseUrlMal = "https://api.jikan.moe/v4";
@@ -11,11 +12,19 @@ const Account = () => {
 	const { state } = useLocation();
 	const [favorites, setFavorites] = useState<any[]>([]);
 	const [updates, setUpdates] = useState<any[]>([]);
+	const [userMangaData, setUserMangaData] = useState<any[]>([]);
 
 	useEffect(() => {
 		console.log(state.account);
 		setUpdates(state.account["updates"]["manga"]);
 		setFavorites(state.account["favorites"]["manga"]);
+		setUserMangaData(state.account["statistics"]["manga"]);
+		setUserMangaData(
+			Object.keys(state.account["statistics"]["manga"]).map((key) => [
+				key,
+				state.account["statistics"]["manga"][key],
+			])
+		);
 	}, []);
 	return (
 		<div
@@ -28,13 +37,72 @@ const Account = () => {
 				alignItems: "center",
 			}}
 		>
-			<div>
-				<Typography color='white'>{state.account["username"]}</Typography>
-			</div>
-			<div style={{ width: "50%" }}>
-				<Typography color='white' align='center'>
-					Updated
+			<div
+				style={{
+					alignSelf: "flex-start",
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-evenly",
+					alignItems: "flex-start",
+				}}
+			>
+				<Card>
+					<CardMedia
+						sx={{ width: "250px", height: "250px" }}
+						image={state.account["images"]["jpg"]["image_url"]}
+					/>
+				</Card>
+				<Typography
+					color='white'
+					sx={{
+						paddingLeft: "20px",
+						maxWidth: "300px",
+
+						height: "250px",
+					}}
+				>
+					{state.account["username"]} <br />
+					About:&nbsp;
+					{state.account["about"]}
+					<br />
+					Gender:&nbsp;
+					{state.account["gender"]}
+					<br />
+					Birthday:&nbsp;
+					{state.account["birthday"]}
+					<br />
 				</Typography>
+
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						alignItems: "center",
+						flexWrap: "wrap",
+						width: "600px",
+						height: "150px",
+						padding: "10px",
+					}}
+				>
+					{userMangaData.map((current) => (
+						<Typography color='white' sx={{ padding: "8px" }}>
+							{current[0]}: {current[1]} <br />
+						</Typography>
+					))}
+				</div>
+			</div>
+			<Typography color='white' align='center'>
+				Updated
+			</Typography>
+			<div
+				style={{
+					width: "100%",
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+				}}
+			>
 				<Grid
 					container
 					direction='row'
@@ -78,10 +146,17 @@ const Account = () => {
 					))}
 				</Grid>
 			</div>
-			<div style={{ width: "50%" }}>
-				<Typography color='white' align='center'>
-					Favorites
-				</Typography>
+			<Typography color='white' align='center'>
+				Favorites
+			</Typography>
+			<div
+				style={{
+					width: "100%",
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+				}}
+			>
 				<Grid
 					container
 					direction='row'
