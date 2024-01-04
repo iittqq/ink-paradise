ab05a1bb16a25d02c07119b9f4130c7d7ba5a380
Adam <ajdupuy15@hmail.com>
2024-01-04 15:43:36 -0600

    Added MyAnimeList.java to the backend and connected it to the frontend.

--- src/api/MalApi.ts
+++ src/api/MalApi.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.jikan.moe/v4";
+const BASE_URL = "http://localhost:8080";
 
 import { TopManga } from "../interfaces/MalInterfaces";
 
 async function fetchAccountData(username: string): Promise<object[]> {
 	try {
-		const response = await axios.get(`${BASE_URL}/users/${username}/full`);
+		const response = await axios.get(`${BASE_URL}/my-anime-list/fetch-account-data`, {
+			params: {
+				username: username
+			},
+		});
+		console.log(response.data);
 		return response.data["data"];
 	} catch (error) {
 		console.error("Error fetching manga:", error);
@@ -16,10 +21,9 @@ async function fetchAccountData(username: string): Promise<object[]> {
 
 async function fetchTopManga(): Promise<TopManga[]> {
 	try {
-		const response = await axios.get(`${BASE_URL}/top/manga`, {
+		const response = await axios.get(`${BASE_URL}/my-anime-list/fetch-top-manga`, {
 			params: {
 				limit: 10,
-				order: { relevance: "desc" },
 			},
 		});
 
